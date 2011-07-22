a90df6b973ef25a3267e6edb9539d5dc61b29b99
Lincoln Nguyen <lincolnn07@gmail.com>
2011-07-22 14:12:57 -0700

    Collective Intelligence update

--- Collective_Intelligence/docclass.py
+++ Collective_Intelligence/docclass.py
@@ -23,7 +23,8 @@ def getwords(doc):
   
 class classifier:
 
-    def __init__(self, getfeatures, filename=None):
+    def __init__(self,getfeatures,filename=None):
+        
     # Counts of feature/category combinators in dictionary
         self.fc = {}
         # Counts of documents in each category
@@ -89,4 +90,112 @@ class classifier:
 
         # Calculate the weighted average
         bp = ((weight * ap) + (totals * basicprob)) / (weight + totals)
-        return bp                       
+        return bp  
+
+class naivebayes(classifier):  # subclass of classifier class above
+    def __init__(self,getfeatures):
+        classifier.__init__(self,getfeatures)   # Set up thresholds and add new instance var to classifier
+        self.thresholds = {}
+        
+    def docprob(self, item, cat):
+        features = self.getfeatures(item)    # get a dictionary from the getwords f
+
+        # Multiply the probabilities of all the features together
+        p = 1
+        for f in features: p*= self.weightedprob(f, cat, self.fprob) #self used to connect method
+        return p                             
+
+    def prob(self, item, cat):
+        catprob = self.catcount(cat) / self.totalcount() # probability of category
+        docprob = self.docprob(item, cat) # probability of document
+        return docprob * catprob      
+
+    # Simple methods to set and get the values returning 1.0 as default
+    def setthreshold(self, cat, t):
+        self.thresholds[cat] = t
+    
+    def getthreshold(self,cat):
+        if cat not in self.thresholds: return 1.0
+        return self.thresholds[cat]
+
+    def classify(self, item, default = None):
+        probs = {}
+        # Find the category with the highest probability
+        max = 0.0
+        for cat in self.categories():    # for loop category in     
+            probs[cat] = self.prob(item, cat)
+            if probs[cat] >max:
+                max = probs[cat]
+                best = cat    
+
+        # Make sure the probability exceeds threshold*next best
+        for cat in probs:
+          if cat==best: continue
+          if probs[cat]*self.getthreshold(best)>probs[best]: return default
+        return best
+
+## Fisher method
+
+class fisherclassifier(classifier):
+        
+    def cprob(self, f, cat):
+        # The frequency fo this feature in this category
+        clf = self.fprob(f, cat)
+        if clf == 0: return 0
+
+        # Frequency of this feature in all categories
+        freqsum = sum([self.fprob(f, c) for c in self.categories()])
+
+        # The probability is the frequency in this category divided by 
+        # the overall frequency
+        p = clf/ (freqsum)
+
+        return p
+            
+    def fisherprob(self, item, cat):
+        # Multiply all the probabilties together
+        p = 1
+        features = self.getfeatures(item)
+        for f in features:
+            p *= (self.weightedprob(f, cat, self.cprob))
+
+        # Take the natural log and multiply by -2
+        fscore = -2 * math.log(p)
+
+        # Use the inverse chi2 function to get a probability
+        return self.invchi2(fscore, len(features) * 2)                                 
+
+    def invchi2(self, chi, df):
+        m = chi / 2.0
+        sum = term = math.exp(-m)
+        for i in range(1, df//2):
+            term *= m / i
+            sum += term
+        return min(sum, 1.0)   
+
+    # Init method with another variable to store cutoffs
+    def __init__(self, getfeatures):
+        classifier.__init__(self, getfeatures)
+        self.minimums = {}                   
+
+    # Methods for getting and setting these cutoff values with a default val = 0
+    def setminimum(self, cat, min):
+        self.minimums[cat] = min
+
+    def getminimum(self, cat):
+        if cat not in self.minimums: return 0
+        return self.minimums[cat]
+
+    # Method to calculate the probs for each category and determine best result
+    # that exceeds the minimum
+    def classify(self, item, default=None):
+        # Loop through looking for the best result
+        best = default
+        max = 0.0
+        for c in self.categories():
+          p = self.fisherprob(item, c)
+          # Make sure it exceeds its minimum
+          if p > self.getminimum(c) and p > max:
+            best = c
+            max = p
+        return best                                                           
